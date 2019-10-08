8e6c36ca76a36e7f9d006d789838ea4f6bd0c388
tomgtbst <tom.gt@foxmail.com>
2019-10-08 19:07:45 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+import AddEntry from './components/AddEntry'
+
+export default function App() {
+  return (
+    <View>
+      <AddEntry />
+    </View>
+  )
+}
+
